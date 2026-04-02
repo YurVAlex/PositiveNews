bbef9f7774fb0b532e1855f2fb3073e1984f17f6
Yurva Va <Yura-va@tut.by>
2026-04-02 17:58:47 +0300

    029 Validator improved

--- src/PositiveNews.Infrastructure/Services/FeedItemValidator.cs
+++ src/PositiveNews.Infrastructure/Services/FeedItemValidator.cs
@@ -1,14 +1,20 @@
 ﻿using HtmlAgilityPack;
+using System.Net;
 using System.Xml.Linq;
 
 public class FeedItemValidator : IFeedItemValidator
 {
     private static readonly XNamespace ContentNs = "http://purl.org/rss/1.0/modules/content/";
+    private static readonly XNamespace DcNs = "http://purl.org/dc/elements/1.1/";
+
     public bool IsValid(XElement itemElement) //TODO: Additional validation
     {
        var fields = new[] { "title", "link", "description", ContentNs + "encoded" };
 
         if (fields.Any(t => string.IsNullOrWhiteSpace(itemElement.Element(t)?.Value)))
+            return false;
+
+        if (itemElement.Element(DcNs + "creator")?.Value == "tinybuddha")
             return false;
 
         var html = itemElement.Element(ContentNs + "encoded")!.Value;
